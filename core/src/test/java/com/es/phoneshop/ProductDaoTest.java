b315d819ac7a462cee219261a2ea65238326c60f
Dzmitry Ramantsevich <d.ramantsevich@gmail.com>
2022-06-07 13:16:23 +0300

    implement ProductDaoTest

--- core/src/test/java/com/es/phoneshop/ProductDaoTest.java
+++ core/src/test/java/com/es/phoneshop/ProductDaoTest.java
@@ -4,6 +4,9 @@ import com.es.core.model.phone.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -13,7 +16,6 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -72,13 +74,65 @@ public class ProductDaoTest {
     @Test
     public void testThatActualPhoneHasImportantFields() {
         List<Phone> phoneList = phoneDao.findWithLimit(0, 10);
-        Phone phoneWithMultiColor = phoneList.get(6);
+        Phone phoneWithMultiColor = phoneList.get(5);
         Phone phone = createPhone();
 
         assertThat(Objects.equals(phoneWithMultiColor.getId(), phone.getId())).isTrue();
         assertThat(Objects.equals(phoneWithMultiColor.getBrand(), phone.getBrand())).isTrue();
         assertThat(Objects.equals(phoneWithMultiColor.getModel(), phone.getModel())).isTrue();
         assertThat(Objects.equals(phoneWithMultiColor.getPrice(), phone.getPrice())).isTrue();
+    }
+
+    @Test
+    public void testGetPhoneByIdWithStock() {
+        Stock phone = phoneDao.getPhoneById(1011L);
+
+        assertThat(phone.getStock()).isGreaterThan(0);
+        assertThat(phone.getPhone().getId()).isEqualTo(1011L);
+    }
+
+    @Test
+    public void testFindAll() {
+        Page<Stock> stockPage = createPageWithStockPhones();
+        Stock phoneWithStock = createPhoneWithStock();
+
+        assertThat(stockPage.stream().anyMatch(p -> p.getPhone().getId()
+                .equals(phoneWithStock.getPhone().getId()))).isTrue();
+    }
+
+    @Test
+    public void testFindAllWithStock() {
+        List<Stock> stockPhones = phoneDao.findAllWithStock();
+
+        assertThat(stockPhones.size()).isEqualTo(3030);
+    }
+
+    @Test
+    public void testFindByKeyword() {
+        int pageNo = 2;
+        int size = 10;
+        String keyword = "Samsung";
+
+        Pageable firstPage = PageRequest.of(pageNo - 1, size);
+        Page<Stock> stockPage = phoneDao.findByKeyword(firstPage, keyword);
+
+        assertThat(stockPage.stream().allMatch(p -> p.getPhone().getModel()
+                .contains(keyword))).isTrue();
+    }
+
+    @Test
+    public void testFindSortedPhonesByKeyword() {
+        int pageNo = 2;
+        int size = 10;
+        String keyword = "Samsung";
+        String property = "displaysizeinches";
+        String direction = "desc";
+
+        Pageable firstPage = PageRequest.of(pageNo - 1, size);
+        Page<Stock> stockPage = phoneDao.findSortedPhonesByKeyword(firstPage, property, direction, keyword);
+
+        assertThat(stockPage.stream().allMatch(p -> p.getPhone().getModel()
+                .contains(keyword))).isTrue();
     }
 
     private Set<Color> createColor() {
@@ -102,5 +156,35 @@ public class ProductDaoTest {
         phone.setPrice(new BigDecimal("99.0"));
 
         return phone;
+    }
+
+    private Stock createPhoneWithStock() {
+        Set<Color> colorSet = new HashSet<>();
+        Color white = new Color(1001L, "White");
+        Color black = new Color(1000L, "Black");
+        colorSet.add(white);
+        colorSet.add(black);
+
+        Phone phone = new Phone();
+        phone.setId(1413L);
+        phone.setBrand("Alcatel");
+        phone.setModel("Alcatel OneTouch Idol X+");
+        phone.setPrice(new BigDecimal("330.0"));
+        phone.setColor(colorSet);
+
+        Stock phoneWithStock = new Stock();
+        phoneWithStock.setPhone(phone);
+        phoneWithStock.setStock(6);
+        phoneWithStock.setReserved(6);
+
+        return phoneWithStock;
+    }
+
+    private Page<Stock> createPageWithStockPhones() {
+        int pageNo = 23;
+        int size = 10;
+
+        Pageable firstPage = PageRequest.of(pageNo - 1, size);
+        return phoneDao.findAll(firstPage);
     }
 }
