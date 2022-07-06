2b53e061d6eaecc08a5c8811e8bc2440cb126231
Dzmitry Ramantsevich <d.ramantsevich@gmail.com>
2022-07-06 16:18:25 +0300

    refactor CartPageController

--- web/src/main/java/com/es/phoneshop/web/controller/pages/CartPageController.java
+++ web/src/main/java/com/es/phoneshop/web/controller/pages/CartPageController.java
@@ -25,19 +25,14 @@ public class CartPageController {
     private CartService cartService;
 
     @Resource
-    DefaultCartService defaultCartService;
+    private DefaultCartService defaultCartService;
 
     @GetMapping
     public String getCart(HttpServletRequest request, Model model) {
         Cart cart = cartService.getCart(request);
 
-        ValidList cartDTOList = new ValidList();
-        List<CartDTO> cartList = cart.getItems()
-                .stream()
-                .map(i -> new CartDTO(i.getStock().getPhone().getId(), i.getStock(), i.getQuantity()))
-                .collect(Collectors.toList());
+        ValidList cartDTOList = getCartDTOValidList(cart);
 
-        cartDTOList.setList(cartList);
         model.addAttribute("cartDTOList", cartDTOList);
         model.addAttribute("cart", cart);
 
@@ -54,43 +49,63 @@ public class CartPageController {
     }
 
     @PostMapping(value = "/update")
-    public String updateCart(
-            @ModelAttribute("cartDTOList") @Valid ValidList cartDTOValidList,
-            BindingResult result,
-            Model model,
-            HttpServletRequest request) {
+    public String updateCart(@ModelAttribute("cartDTOList") @Valid ValidList cartDTOValidList, BindingResult result,
+                             Model model, HttpServletRequest request) {
         Cart cart = cartService.getCart(request);
+        ValidList validList;
 
         if (result.hasErrors()) {
-            List<CartDTO> cartListWithoutQuantity = cart.getItems()
-                    .stream()
-                    .map(i -> new CartDTO(i.getStock().getPhone().getId(),
-                            defaultCartService.getPhone(i.getStock().getPhone().getId())))
-                    .collect(Collectors.toList());
-            List<CartDTO> cartList = IntStream.range(0, cartDTOValidList.getList().size())
-                    .mapToObj(i -> new CartDTO(cartListWithoutQuantity.get(i).getItemId(),
-                            cartListWithoutQuantity.get(i).getStock(),
-                            cartDTOValidList.getList().get(i).getQuantity()))
-                    .collect(Collectors.toList());
-
-            cartDTOValidList.setList(cartList);
-            model.addAttribute("cart", cart);
-            model.addAttribute("cartDTOList", cartDTOValidList);
-
-            return "cart";
+            validList = getCartDTOValidListHasErrors(cart, cartDTOValidList);
         } else {
             try {
-                for (CartDTO item : cartDTOValidList.getList()) {
-                    cartService.update(cart, item.getItemId(), item.getQuantity());
-                }
+                cartDTOValidList.getList()
+                        .forEach(item -> cartService.update(cart, item.getItemId(), item.getQuantity()));
 
                 model.addAttribute("cart", cart);
                 model.addAttribute("cartDTOList", cartDTOValidList);
 
                 return "redirect:/cart";
             } catch (OutOfStockException e) {
-                return "error";
+                validList = getCartDTOValidListHasErrors(cart, cartDTOValidList);
+
+                model.addAttribute("errorMessage", "Out of stock");
             }
         }
+        model.addAttribute("cart", cart);
+        model.addAttribute("cartDTOList", validList);
+
+        return "cart";
+    }
+
+    private ValidList getCartDTOValidList(Cart cart) {
+        ValidList cartDTOList = new ValidList();
+
+        List<CartDTO> cartList = cart.getItems()
+                .stream()
+                .map(i -> new CartDTO(i.getStock().getPhone().getId(),
+                        i.getStock(),
+                        i.getQuantity()))
+                .collect(Collectors.toList());
+
+        cartDTOList.setList(cartList);
+
+        return cartDTOList;
+    }
+
+    private ValidList getCartDTOValidListHasErrors(Cart cart, ValidList cartDTOValidList) {
+        List<CartDTO> cartListWithoutQuantity = cart.getItems()
+                .stream()
+                .map(i -> new CartDTO(i.getStock().getPhone().getId(),
+                        defaultCartService.getPhone(i.getStock().getPhone().getId())))
+                .collect(Collectors.toList());
+        List<CartDTO> cartList = IntStream.range(0, cartDTOValidList.getList().size())
+                .mapToObj(i -> new CartDTO(cartListWithoutQuantity.get(i).getItemId(),
+                        cartListWithoutQuantity.get(i).getStock(),
+                        cartDTOValidList.getList().get(i).getQuantity()))
+                .collect(Collectors.toList());
+
+        cartDTOValidList.setList(cartList);
+
+        return cartDTOValidList;
     }
 }
